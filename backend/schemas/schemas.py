a00422b62670ae672f83523159cb67364e0e7b04
goliveira1 <goliveira1@uolinc>
2025-05-05 22:23:39 -0300

    criado modelo de entrada de cadastro e modelo de entrada do cadastro no bd

--- backend/schemas/schemas.py
+++ backend/schemas/schemas.py
@@ -10,3 +10,8 @@ class CadastroUserInput(BaseModel):
     senha : str = Field(..., min_length=6, max_length=20)
     confirmar_senha : str = Field(..., min_length=6, max_length=20)
     
+class CadastroUser(BaseModel):
+    nome: str = Field(..., min_length=3, max_length=50)
+    email : str = EmailStr
+    senha : str = Field(..., min_length=6, max_length=20)
+
